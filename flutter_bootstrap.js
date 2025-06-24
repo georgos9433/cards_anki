f9f62e05ec681b70bae329a14f7fc88c65698b91
georgos9433 <georgos9433@gmail.com>
2025-06-24 22:44:18 +0200

    update categories page

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2135445714"
+    serviceWorkerVersion: "860215202"
   }
 });
